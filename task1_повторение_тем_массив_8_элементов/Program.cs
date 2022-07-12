c40649713534c9be228b9bed257a9da207ee0e59
Asia84 <84asia@mail.ru>
2022-07-12 17:30:56 +0700

    задача повторение массива из 8 элементов, ввод пользователем

---
+++ task1_повторение_тем_массив_8_элементов/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введи количество элементов в массиве: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+for(int i = 0; i < n; i++)
+{
+    Console.WriteLine("Введи число: ");
+    array[i] =  Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("["+ string.Join(", ", array) + "]");

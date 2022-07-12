5d8427316dcd922b62b7c64131a8c1298b22b506
Asia84 <84asia@mail.ru>
2022-07-13 00:44:13 +0700

    ДИНАМИЧЕСКИЙ МАССИВ

--- task42_десятичное_в_двоичное/Program.cs
+++ task42_десятичное_в_двоичное/Program.cs
@@ -18,3 +18,32 @@ while(n > 0)
 Console.WriteLine($"Десятичное число {nn} в двоичной системе равно: {binary}");
 
 
+/*
+List<int> array = new List<int>();//массив список безразмерный
+array.Add(6);
+array.Add(7);
+array.Add(7);
+array.Add(7);
+array.Add(7);
+array.Add(7);
+Console.WriteLine(string.Join(", ",array));
+*/
+
+
+/*ЧЕРЕЗ ДИНАМИЧЕСКИЙ МАССИВ
+
+int count = 0, x, n = Convert.ToInt32(Console.ReadLine());
+List<int> array = new List<int>();//динамический массив
+while (n > 0)
+{
+x = n % 2;
+array.Add(x);
+count++;
+//Console.Write(x);
+n /= 2;
+}
+for (int i = count - 1; i >= 0; i--)
+{
+Console.Write(array[i]);
+}
+*/

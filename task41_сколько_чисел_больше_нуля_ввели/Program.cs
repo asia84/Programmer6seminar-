8f9840d9957090710377d731b41db12037a81b36
Asia84 <84asia@mail.ru>
2022-07-13 22:38:37 +0700

    Посчитали количество положительных чисел

---
+++ task41_сколько_чисел_больше_нуля_ввели/Program.cs
@@ -0,0 +1,21 @@
+﻿ /*41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0,7,8,-2,-2-> 2
+1,-7,567,89,223-> 4
+*/
+
+Console.WriteLine("сколько чисел вы хотите ввести: ");
+int size = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите числа: ");
+int countN = 0;
+int i;
+int [] n = new int[size];
+
+for(i = 0; i < size; i++)
+{
+    n[i] = Convert.ToInt32(Console.ReadLine());
+    if (n[i]>0) 
+    {
+        countN++;
+    }
+}
+Console.WriteLine($"вы ввели {countN} положительных чисел");
